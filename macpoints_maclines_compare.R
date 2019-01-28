7aba9ef21a51393e0338f358f7214271349a59ea
rtrichler <rbtrichler@ip-128-239-186-159.v4.wm.edu>
2019-01-28 15:05:39 -0500

    More descriptive stuff

--- macpoints_maclines_compare.R
+++ macpoints_maclines_compare.R
@@ -33,6 +33,11 @@ unique(maclines$project_id)
 sum(is.na(maclines$project_id))
 sum(is.na(mac_spdf$project_id))
 
+#output text
+maclines$Geocoding.and.Review.Note[49]
+#identify projects where project_id is NA (and most other fields are also NA)
+maclines$id[is.na(maclines$project_id)]
+
 #identify projects in maclines that are only coded to ADM0 or ADM1
 maclines_adm01 <- maclines[grep("KHM_ADM", maclines$GeoJSON.Link.or.Feature.ID),]
 table(maclines_adm01$project_id)
@@ -40,4 +45,13 @@ table(maclines_adm01$project_id)
 #identify projects in maclines that are ODA-like flow-class
 maclines_ODA<-maclines[grep("ODA-like",maclines$flow_class),]
 table(maclines_ODA$project_id)
+
+#subset maclines to non-ADM0 and non-ADM1
+maclines_prec<- maclines[-grep("KHM_ADM", maclines$GeoJSON.Link.or.Feature.ID),]
+# identify projects in maclines_prec where dates are missing
+maclines_prec$project_id[is.na(maclines$start_actual)]
+sum(is.na(maclines_prec$end_actual))
+
+
+
 
